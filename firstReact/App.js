47eab9a6ba90a852cbee95e669fbb9c976c4669e
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-10-07 09:49:30 +0700

    second commit

---
+++ firstReact/App.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+export default function App() {
+    const [advice, setAdvice] = useState("")
+    const [count, setCount] = useState(0)
+    async function getAdvice() {
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+        setAdvice(data.slip.advice);
+        setCount((c) => c + 1);
+    }
+    useEffect(function () {
+        getAvice();
+    }, []);
+    return <div>
+        <h1>{advice} </h1>
+        <button onClick={getAdvice}>Learning React</button>
+        <Message count={count} />
+    </div>
+}
+function Message(props) {
+    return (
+        <p>
+            You have read{props.count}pieces of advice
+        </p>
+    )
+}
